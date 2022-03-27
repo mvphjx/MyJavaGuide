d1ee0f2f51a3f591371182fbfb041c4fb00385f8
hjx <hanjianxiang@hisign.com.cn>
2022-03-27 18:32:49 +0800

    观察者模式

--- common/src/main/java/com/guide/common/design/foraction/observer/Subject.java
+++ common/src/main/java/com/guide/common/design/foraction/observer/Subject.java
@@ -2,10 +2,11 @@ package com.guide.common.design.foraction.observer;
 
 public interface Subject
 {
+    //注册
     void registerObserver(Observer observer);
-
+    //取消
     void removeObserver(Observer observer);
-
+    //通知
     void notifyObservers(Message message);
 
 }
